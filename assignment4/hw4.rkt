c23b9079d36b4a14c648f512c6b0d05e404232a9
liihhnm <davididota@gmail.com>
2016-11-12 03:26:36 +0800

    hw4.rkt

--- assignment4/hw4.rkt
+++ assignment4/hw4.rkt
@@ -1,4 +1,3 @@
-
 #lang racket
 
 (provide (all-defined-out)) ;; so we can put tests in a second file
@@ -36,7 +35,42 @@
 (define (dan-dog items)
   (cons (car items) (lambda () (dan-dog (cons (cdr items) (car items))))))
 (define dan-then-dog (lambda () (dan-dog (cons "dan.jpg" "dog.jpg"))))
-  
+
+(define (stream-add-zero s)
+  (letrec ([assist (lambda (s) (cons (cons 0 (car (s)))
+                                  (lambda () (assist (cdr (s))))))])
+    (lambda () (assist s))))
+
+(define (cycle-lists xs ys)
+  (letrec ([assist (lambda (n) (cons (cons (list-nth-mod xs n)
+                                           (list-nth-mod ys n))
+                                     (lambda () (assist (+ n 1)))))])
+    (lambda () (assist 0))))
+
+(define (vector-assoc v vec)
+  (letrec ([assist (lambda (n) (cond
+                                 [(= n (vector-length vec)) #f]
+                                 [(not (pair? (vector-ref vec n))) (assist (+ n 1))]
+                                 [#t (if (equal? v (car (vector-ref vec n)))
+                                         (vector-ref vec n)
+                                         (assist (+ n 1)))]))])
+    (assist 0)))
+
+;before seeing the answer, have no idea about the exist of function (vector-assoc v vector)
+;and not see it in document
+(define (cached-assoc xs n)
+  (letrec ([position 0]
+           [cache (make-vector n (cons #f #f))]
+           [f (lambda (v) (let ([ans (assoc v (vector->list cache))])
+                            (if ans
+                                ans
+                                (let ([new-ans (assoc v xs)])
+                                  (if (not new-ans)
+                                      new-ans
+                                      (begin (vector-set! cache position new-ans)
+                                             (set! position (remainder (+ position 1) n))
+                                             new-ans))))))])
+    f))
 
 
 
